8bc97b37f81b2a98900bd47aedf50c77de6c47d3
sandhya6465 <sandhyakumari20082003@example.com>
2025-02-17 14:31:24 +0530

    template

---
+++ script.js
@@ -0,0 +1,4 @@
+function flipImage() {
+    const cardInner = document.querySelector('.flip-card-inner');
+    cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
+}
